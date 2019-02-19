ebe88d4316cfc823273213e8cd66a5f0e014a59e
Tiago59510 <esteprowork@gmail.com>
2019-02-19 14:28:08 +0100

    ajout des integration dans app.php

--- private/app/src/app.php
+++ private/app/src/app.php
@@ -1,1 +1,20 @@
-yop l'appli démarre
+<?php
+
+//integration de la configuration
+require_once "../private/app/config/config.php";
+
+//definition de l'environnement
+require_once "../private/app/src/environnement.php";
+
+//Comportement des erreurs
+require_once "../private/app/src/err_reporting.php";
+
+//Connexions aux bases de données
+require_once "../private/app/src/db_connect.php"
+
+//Routage de l'application
+require_once "../private/app/src/routing.php"
+
+//
+require_once "../private/app/src/compile.php"
+?>  
