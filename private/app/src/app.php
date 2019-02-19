d9b665271238161af347bf7a1a74606d514ff977
Tiago59510 <esteprowork@gmail.com>
2019-02-19 12:46:50 +0100

    ajout du fichier de demarrage de l'application

---
+++ private/app/src/app.php
@@ -0,0 +1,1 @@
+yop l'appli démarre
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * FICHIER DE DEMARRAGE DE LAPPLICATION
+ */
+
+ // 1. Integration du fichier d'execution de l'application
+ require_once "../private/app/src/app.php";
