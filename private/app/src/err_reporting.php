209c3aca708dbe47c0c27e0c8167a8db8b434b76
Tiago59510 <esteprowork@gmail.com>
2019-02-20 10:36:58 +0100

    err modif

--- private/app/src/err_reporting.php
+++ private/app/src/err_reporting.php
@@ -1,5 +1,21 @@
 <?php
 
+//Fichier de definition du comportement des erreurs php
+
+
+//Dans le cas ou la variable $env n'est pas défini 
+//On initialise la variable $env avec la valeur prod
+
+if (!isset($env)){
+    $env = prod;
+}
+
+//Dans le cas ou la variable $env est défini à une autre valeur que "dev"
+//On demande à php d'ignorer toutes les erreurs.
+
+
+
+
 
 
 ?>
